6505f8e592523f47b9cddd897c8337367bd36afc
sourabh200130 <shettysourabh2001@gmail.com>
2023-02-14 12:14:50 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from "./assets/download.jpg";
+import Demo from './components/demo';
 
 function App() {
+  var x=10;
+  var text;
+  if(x===20){
+    text = "Equal";
+  }
+  else{
+    text = "not Equal"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> hello world</h1>
+     <h2>{50+5}</h2>
+     <img src={Image} alt="pic"/>
+     <h2> {text} </h2>
+     <h3>{x>=10 ? "greater than or equal to 10" : "not equal to 10"}</h3>
+    <Demo />
     </div>
   );
 }
