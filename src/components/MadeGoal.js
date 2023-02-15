d1d6e9c63f6811fdbfdcb1ba95da81735e73e637
sourabh200130 <shettysourabh2001@gmail.com>
2023-02-15 15:10:14 +0530

    15th

---
+++ src/components/MadeGoal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MadeGoal = () => {
+    return(
+        <div>
+        <h1>made goal</h1>
+        </div>
+    );
+};
+export default MadeGoal;
--- src/components/demo.js
+++ src/components/demo.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Demo2 from "./Demo2";
-
+import MadeGoal from "./MadeGoal";
+import MissedGoal from "./missedGoal";
 const Demo= (props) => {
-    return <div> 
+    const x =10;
+    const arr = ["a","b","c","d","e","f"];
+    const handleClick = (x,y) =>{
+        alert(x+y)
+    }
+    return  <div> 
         <Demo2 para2={props.para}/>
         Demo Component
         <h1>{props.para}</h1>
-
+        <button onClick ={() => handleClick(10,50)}>click</button>
+        <button onClick={() => handleClick(100,500)}>and more</button>
+        <Demo2 para2={props.para}/>
+        {x>=10 ? <MadeGoal /> : <MissedGoal />}
+        <ol>
+            {arr.map((alph, i) => (
+                <li key={i}>{alph}</li>
+            ))}
+        </ol>
     </div>;
 
 };
