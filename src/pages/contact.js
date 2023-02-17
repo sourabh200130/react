d7afa0f326469c1df006459b403767a13d9f05f4
sourabh200130 <shettysourabh2001@gmail.com>
2023-02-17 12:57:46 +0530

    useEffect

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 
 const Contact = () => {
     const[isName,setIsName]=useState(" ")
     const[isPassword,setIsPassword]= useState("")
+    const [count,setCount]=useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+           setCount((count)  => count+1);
+    },1000);
+        return() =>clearInterval(interval);
+},[])
     const handleNameChange = (event) =>{
         setIsName(event.target.value)
     }
@@ -18,13 +26,14 @@ const Contact = () => {
     return(
         <div>
             <NavigationBar />
+            <h1>{count}</h1>
             <h1>Contact page Component</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>name :</label>
                 <input type="text" name="name" onChange={handleNameChange} />
                 <label>Password :</label>
                 <input type="password" name="password" onChange={handlePasswordChange} />
-                <input type="submit" value="Submit" onChange={handleSubmit} />
+                <input type="submit" value="Submit" />
             </form>
         </div>
     );
