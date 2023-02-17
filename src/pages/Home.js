a01bad25df1716b2998b2b09236cc489b31454a6
sourabh200130 <shettysourabh2001@gmail.com>
2023-02-17 12:33:43 +0530

    hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationBar from "../components/NavigationBar";
+import "./Style.css"
+import classes from "./Style.module.css";
 
 const Name = () => {
+    const [isData, setIsData]= useState("hello")
+    const myStyle={
+        color:"red",
+        backgroundColor:"blue",
+        textAlign:"center",
+    };
+    const handleChange=()=>{
+        setIsData("World");
+    }
     return(
         <div>
+            <h1>{isData}</h1>
+            <button on onClick={handleChange}>change</button>
             <NavigationBar />
-            <h1>Home page Component</h1>
+            <h1 style={{color: "white", backgroundColor: "black"}}>Home page Component</h1>
+            <h1 style={myStyle}>jbcsai</h1>
+            <h3 className="demo">pogo</h3>
+            <h4 className={classes.body}>yo</h4>
+            <h5 className={`${classes.body} demo`}> add more classes</h5>
         </div>
     );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 
 const Contact = () => {
+    const[isName,setIsName]=useState(" ")
+    const[isPassword,setIsPassword]= useState("")
+    const handleNameChange = (event) =>{
+        setIsName(event.target.value)
+    }
+    const handlePasswordChange = (event) =>{
+        setIsPassword(event.target.value)
+    }
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(isName,isPassword)
+    }
+
     return(
         <div>
             <NavigationBar />
             <h1>Contact page Component</h1>
+            <form>
+                <label>name :</label>
+                <input type="text" name="name" onChange={handleNameChange} />
+                <label>Password :</label>
+                <input type="password" name="password" onChange={handlePasswordChange} />
+                <input type="submit" value="Submit" onChange={handleSubmit} />
+            </form>
         </div>
     );
 };
